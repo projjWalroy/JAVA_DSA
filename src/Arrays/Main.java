f158fe31939672e39c654a96cf918b5c6d7375d8
Projjwal Roy <64724432+projjWalroy@users.noreply.github.com>
2022-09-23 13:29:15 +0530

    started sorting

---
+++ src/Arrays/Main.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class Main {
+    /*Java uses stable algorithom based on mergesort adaptation of timsort
+    for non primitive data types
+    and dual pivot quick sort for primitive data types
+    Arrays.sort(array_name,start_idx,end_idx);
+    1. class class_name implements Comparable<class_name>
+    2. class class_name implements Comparator<class_name>
+       it takes two object
+       public int compare(){}
+       Arrays.sort(arr,new class_name());
+       for comparator we have to create two class
+     Collection implements comparable function
+     */
+}
